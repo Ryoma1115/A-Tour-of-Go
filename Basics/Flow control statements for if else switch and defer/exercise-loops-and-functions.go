2d18615cfaf3670915e0fed0a947818cd42342dd
github Ryoma1115 <t9831703@gmail.com>
2020-09-01 17:14:04 +0900

    Finished untill exercise-loops-and-functions.go

---
+++ Basics/Flow control statements for if else switch and defer/exercise-loops-and-functions.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func Sqrt(x float64) float64 {
+	// z := 1.0
+	// for i := 0; i < 10; i++ {
+	// 		z -= (z*z - x) / (2 * z)
+	// }
+	z := 1.0
+	for d := 1.0; d*d > 1e-10; z -= d {
+		d = (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
